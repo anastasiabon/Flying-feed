d0b6165bf3419e99d74d5c6c7c6b053daf3427ba
anastasiabon <avbondarkova@mail.ru>
2017-09-14 16:38:04 +0300

    Add page not found comp

--- app/components/Main.js
+++ app/components/Main.js
@@ -3,11 +3,14 @@ import Feed from './feed'
 import Admin from './admin'
 import { Switch, Route } from 'react-router-dom'
 
+const Error404 = () => (<h1>Page not found</h1>)
+
 const Main = () => (
   <main>
     <Switch>
       <Route exact path='/' component={Feed}/>
       <Route path='/admin' component={Admin}/>
+      <Route path='*' exact={true} component={Error404} />
     </Switch>
   </main>
 )
